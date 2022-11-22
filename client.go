9d03d1c8f73476a7cbc65090e4fb8abbfa919060
Dmitry Sedykh <sedykh@gmail.com>
2022-11-22 12:49:32 +0300

    add log level synonims

--- client.go
+++ client.go
@@ -8,13 +8,31 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-// LogItem является синонимом описания записи лога.
-type LogItem = api.LogItem
+// Экспортируемые синонимы данных.
+type (
+	Item  = api.LogItem       // запись лога
+	Level = api.LogItem_Level // уровень лога
+)
+
+const (
+	// информация
+	INFO Level = api.LogItem_LEVEL_INFO
+	// предупреждение
+	WARNING Level = api.LogItem_LEVEL_WARNING
+	// ошибка
+	ERROR Level = api.LogItem_LEVEL_ERROR
+	// фатальная ошибка
+	FATAL Level = api.LogItem_LEVEL_FATAL
+	// отладочный уровень
+	DEBUG Level = api.LogItem_LEVEL_DEBUG
+	// уровень трассировки
+	TRACE Level = api.LogItem_LEVEL_TRACE
+)
 
 // Receiver возвращает функцию для чтения логов с удалённого сервиса по gRPC.
 // Каждый вызов этой функции возвращает либо запись лога, либо ошибку.
 // В случае планового закрытия канала получения лога сервером возвращается ошибка io.EOF.
-func Receiver(ctx context.Context, cc grpc.ClientConnInterface) (func() (*LogItem, error), error) {
+func Receiver(ctx context.Context, cc grpc.ClientConnInterface) (func() (*Item, error), error) {
 	client := api.NewLogServiceClient(cc)               // инициализируем клиент
 	stream, err := client.Logs(ctx, new(emptypb.Empty)) // инициализируем получение логов
 	if err != nil {
