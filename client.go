40ec104c3ce8d3b02736f04f14cefccb815eb28f
Dmitry Sedykh <sedykh@gmail.com>
2022-11-22 14:03:15 +0300

    add log print function

--- client.go
+++ client.go
@@ -2,11 +2,30 @@ package grpclog
 
 import (
 	"context"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/mdigger/grpclog/api/v1"
+	"github.com/samber/lo"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
+
+// Receiver возвращает функцию для чтения логов с удалённого сервиса по gRPC.
+// Каждый вызов этой функции возвращает либо запись лога, либо ошибку.
+// В случае планового закрытия канала получения лога сервером возвращается ошибка io.EOF.
+func Receiver(ctx context.Context, cc grpc.ClientConnInterface) (func() (*Item, error), error) {
+	client := api.NewLogServiceClient(cc)               // инициализируем клиент
+	stream, err := client.Logs(ctx, new(emptypb.Empty)) // инициализируем получение логов
+	if err != nil {
+		return nil, err
+	}
+
+	return stream.Recv, nil // возвращаем функцию для чтения логов
+}
 
 // Экспортируемые синонимы данных.
 type (
@@ -29,15 +48,45 @@ const (
 	TRACE Level = api.LogItem_LEVEL_TRACE
 )
 
-// Receiver возвращает функцию для чтения логов с удалённого сервиса по gRPC.
-// Каждый вызов этой функции возвращает либо запись лога, либо ошибку.
-// В случае планового закрытия канала получения лога сервером возвращается ошибка io.EOF.
-func Receiver(ctx context.Context, cc grpc.ClientConnInterface) (func() (*Item, error), error) {
-	client := api.NewLogServiceClient(cc)               // инициализируем клиент
-	stream, err := client.Logs(ctx, new(emptypb.Empty)) // инициализируем получение логов
-	if err != nil {
-		return nil, err
+var (
+	levelShortnames = map[Level]string{
+		TRACE:   "TRC",
+		DEBUG:   "DBG",
+		INFO:    "INF",
+		WARNING: "WRN",
+		ERROR:   "ERR",
+		FATAL:   "FTL",
+	}
+	newLineReplacer = strings.NewReplacer("\n", "\\n", "\r", "\\r")
+)
+
+// Print выводит в форматированном виде запись лога в os.Stdout.
+func Print(logmsg *Item) {
+	fmt.Printf("%4d %v %3s %s",
+		logmsg.Id,                             // счётчик
+		time.Now().Local().Format("15:04:05"), // локальное время
+		levelShortnames[logmsg.Level],         // уровень сообщения
+		logmsg.Message)                        // текст сообщения
+
+	// дополнительные поля
+	keys := lo.Keys(logmsg.Fields) // получаем список ключей
+	sort.Strings(keys)             // сортируем его
+
+	// перебираем ключи дополнительного поля
+	for _, k := range keys {
+		// название ключа
+		if strings.ContainsAny(k, " \t\r\n\"") {
+			k = strconv.Quote(newLineReplacer.Replace(k))
+		}
+		fmt.Printf(" %s=", k)
+
+		// значение ключа
+		v := logmsg.Fields[k]
+		if strings.ContainsAny(v, " \t\r\n\"") {
+			v = strconv.Quote(newLineReplacer.Replace(v))
+		}
+		fmt.Print(v)
 	}
 
-	return stream.Recv, nil // возвращаем функцию для чтения логов
+	fmt.Println() // перевод на новую строку в конце
 }
--- go.mod
+++ go.mod
@@ -7,8 +7,11 @@ require (
 	google.golang.org/protobuf v1.28.1
 )
 
+require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/samber/lo v1.35.0
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
